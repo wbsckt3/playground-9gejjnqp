991bd054fd32dbf6db3cf17bbbe52240043bf4f4
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-09-21 14:58:01 -0500

    Update universe4.js

--- nodejs-project/universe4.js
+++ nodejs-project/universe4.js
@@ -1,10 +1,23 @@
 function countAllStars(galaxies) {
-  var totalStars = 0;
-  galaxies.forEach(function(stars) {
-    totalStars = stars; // fix me
-  }, this);
-  return totalStars;
+  var reduceUsed = false; // Inicializa la variable reduceUsed en false
+  var totalStars = galaxies.reduce(function (accumulator, galaxy) {
+    reduceUsed = true; // Establece reduceUsed en true
+    return accumulator + galaxy.stars;
+  }, 0); // 0 es el valor inicial del acumulador
+
+  return { totalStars, reduceUsed }; // Retorna tanto el total de estrellas como el estado de reduceUsed
 }
+
+// Ejemplo de uso
+var galaxies = [
+  { name: 'Galaxy A', stars: 100 },
+  { name: 'Galaxy B', stars: 200 },
+  { name: 'Galaxy C', stars: 300 }
+];
+
+var result = countAllStars(galaxies);
+console.log(result.totalStars); // Esto imprimirá 600
+console.log(result.reduceUsed); // Esto imprimirá true
 
 // { autofold
 module.exports = {
