1317d5a76d9460900185fdaecd470bdf9ed64696
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-09-21 13:32:17 -0500

    Update universe.js

--- nodejs-project/universe.js
+++ nodejs-project/universe.js
@@ -1,3 +1,6 @@
+
+// Supongamos un arreglo que nos indica el  número de estrellas por galaxia = [10, 3, 2, 1, 6, 1, 9, 2]
+
 function countAllStars(galaxies) {
   var totalStars = 0;
   galaxies.forEach(function(stars) {
