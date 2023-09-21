82a9974ed0490b3f4a2dbd09278e8afcf4ae7ee5
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-09-21 15:40:21 -0500

    Update universe4.js

--- nodejs-project/universe4.js
+++ nodejs-project/universe4.js
@@ -5,13 +5,14 @@ const countAllStars = (galaxies) => {
 
 // Ejemplo de uso sobre objetos:  reduce + función flecha
 // Recorrer el objeto y sumar las variables es imposible o por lo menos extremadamente dificil con ciclos o index tradicionales
-// var galaxies = [
-//   { name: 'Galaxy A', stars: 100 },
-//   { name: 'Galaxy B', stars: 200 },
-//   { name: 'Galaxy C', stars: 300 }
-// ];
-
-//console.log(result.totalStars); // Esto imprimirá 600
+// const countAllStars = (galaxies) => {
+//   const totalStars = galaxies.reduce((accumulator, galaxy) => accumulator + galaxy.stars, 0);
+//   return totalStars;
+// };    var galaxies = [
+//         { name: 'Galaxy A', stars: 100 },
+//         { name: 'Galaxy B', stars: 200 },
+//         { name: 'Galaxy C', stars: 300 }
+//  ];  countAllStars( galaxies )
 
 // { autofold
 module.exports = {
