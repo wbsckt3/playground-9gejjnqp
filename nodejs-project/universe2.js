ecba5295a77ae16b0d4ca61abbad1f12960498e3
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-09-21 14:21:41 -0500

    Create universe2.js

---
+++ nodejs-project/universe2.js
@@ -0,0 +1,14 @@
+
+function countAllStars(galaxies) {
+  var totalStars = 0;
+  galaxies.forEach(function(stars) {
+    totalStars = stars; // fix me
+  }, this);
+  return totalStars;
+}
+
+// { autofold
+module.exports = {
+  countAllStars: countAllStars
+};
+// }
