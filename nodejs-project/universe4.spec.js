c8df34359b9da2448ac36583587ccdb25c4b23c2
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-09-21 15:23:37 -0500

    Update universe4.spec.js

--- nodejs-project/universe4.spec.js
+++ nodejs-project/universe4.spec.js
@@ -10,11 +10,7 @@ Array.prototype.reduce = function () {
 
 it('should sum stars', function () {
   try {
-    assert.equal(600, universe.countAllStars( [ 
-      { name: 'Galaxy A', stars: 100 },
-      { name: 'Galaxy B', stars: 200 },
-      { name: 'Galaxy C', stars: 300 }
-    ]));
+    assert.equal(600, universe.countAllStars( [{ name: 'Galaxy A', stars: 100 },{ name: 'Galaxy B', stars: 200 },{ name: 'Galaxy C', stars: 300 }] ));
 
     if (reduceUsed) {
       printMessage('Ok! el uso de Reduce para reducir el tiempo de ejecución. 🙏');
