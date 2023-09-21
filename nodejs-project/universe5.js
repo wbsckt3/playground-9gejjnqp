c89b862a79f06d8bad62f5c6ebb6367303642187
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-09-21 16:15:53 -0500

    Update universe5.js

--- nodejs-project/universe5.js
+++ nodejs-project/universe5.js
@@ -2,6 +2,13 @@ function getGalaxyNames(galaxies) {
   return galaxies.map((galaxy) => galaxy.name);
 }
 
+/*
+const galaxies =  [
+    { name: 'Galaxy A', stars: 100 },
+    { name: 'Galaxy B', stars: 200 },
+    { name: 'Galaxy c', stars: 800 }
+  ]; universe.getGalaxyNames(galaxies)
+*/
 // { autofold
 module.exports = {
   getGalaxyNames: getGalaxyNames
