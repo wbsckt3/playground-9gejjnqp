a698be72b8ccd90b2bb1bbb10c2aa5e2377ff9c1
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-09-21 14:29:33 -0500

    Update universe2.spec.js

--- nodejs-project/universe2.spec.js
+++ nodejs-project/universe2.spec.js
@@ -1,5 +1,5 @@
 ﻿﻿var assert = require('assert');
-var universe = require('./universe.js');
+var universe = require('./universe2.js');
 
 var reduceUsed = false;
 var superReduce = Array.prototype.reduce;
