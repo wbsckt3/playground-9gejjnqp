7733d3c7f98d0b84690f21ce5c992853ae2cb8b2
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-09-21 22:56:03 -0500

    Create universe0.js

---
+++ nodejs-project/universe0.js
@@ -0,0 +1,14 @@
+
+ function countAllStars(galaxies) {
+    var totalStars = 0;
+    for (let i = 0; i < galaxias.length; i++) {
+      totalStars += galaxias[i];
+    }
+    return totalStars;
+} 
+
+// { autofold
+module.exports = {
+  countAllStars: countAllStars
+};
+// }
