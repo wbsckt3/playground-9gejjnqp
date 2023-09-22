be85a22d1f263e3ee7840fdf314bd0f6459589c0
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-09-22 07:40:19 -0500

    Create universe1.1.js

---
+++ nodejs-project/universe1.1.js
@@ -0,0 +1,17 @@
+function hacerAlgo(callback) {
+  // Realiza alguna tarea aquí
+  // Llama al callback cuando la tarea esté completa
+  callback();
+}
+
+function miCallback() {
+  console.log("La tarea se ha completado.");
+}
+
+hacerAlgo(miCallback);
+
+// { autofold
+module.exports = {
+  countAllStars: countAllStars
+};
+// }
