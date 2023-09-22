2de3e68f0b1b608f520412713cebe8e8c2cf5c78
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-09-21 23:12:55 -0500

    Update universe0.js

--- nodejs-project/universe0.js
+++ nodejs-project/universe0.js
@@ -7,6 +7,19 @@
     return totalStars;
 } 
 
+/*
+var resultado = countAllStars(galaxies)
+document.write(resultado);
+// test de velocidad:
+var i = 0;
+galaxies = Array.apply(null, Array(10000)).map(function () { return ++i });
+var count = 10000;
+var start = new Date();
+for (i = 0; i < count; i++) countAllStars(galaxies, 9999);
+var milliseconds = (new Date()) - start;
+document.write("  tiempo en ms: " + milliseconds);
+*/
+
 // { autofold
 module.exports = {
   countAllStars: countAllStars
