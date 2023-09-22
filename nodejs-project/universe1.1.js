8ea66a697b0aec96a8364b25ab7339fd1831c46c
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-09-22 09:42:25 -0500

    Update universe1.1.js

--- nodejs-project/universe1.1.js
+++ nodejs-project/universe1.1.js
@@ -1,15 +1,15 @@
 // Definir una función que retorne una promesa con un retraso
 function delay(ms) {
-  return new Promise(resolve => {
+  return new Promise(function (resolve) {
     setTimeout(resolve, ms);
   });
 }
 
-// Definir una función asíncrona que usa el retraso
-async function myAsyncFunction() {
+function myAsyncFunction() {
   console.log('Comenzando tarea asíncrona...');
-  await delay(4000); // Espera 2 segundos (2000 milisegundos)
-  console.log('Tarea asíncrona completada después de 2 segundos.');
+  return delay(2000).then(function () {
+    console.log('Tarea asíncrona completada después de 2 segundos.');
+  });
 }
 
 // Llamar a la función asíncrona
