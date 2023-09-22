038c10165a55071d295ce1b55ff6a43877236e59
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-09-22 11:54:47 -0500

    Update universe7.js

--- nodejs-project/universe7.js
+++ nodejs-project/universe7.js
@@ -1,10 +1,53 @@
-function countAllStars(galaxies) {
-  var totalStars = 0;
-  galaxies.forEach(function(stars) {
-    totalStars = stars; // fix me
-  }, this);
-  return totalStars;
+function getNestedRecordsExample() {
+  return [
+    {
+      id: 1,
+      name: 'Record 1',
+      details: {
+        description: 'Details for Record 1',
+        nestedInfo: {
+          value: 'Value for Record 1',
+        },
+      },
+    },
+    {
+      id: 2,
+      name: 'Record 2',
+      details: {
+        description: 'Details for Record 2',
+        nestedInfo: {
+          value: 'Value for Record 2',
+        },
+      },
+    },
+    {
+      id: 3,
+      name: 'Record 3',
+      details: {
+        description: 'Details for Record 3',
+        nestedInfo: {
+          value: 'Value for Record 3',
+        },
+      },
+    },
+  ];
 }
+
+function getNestedInfo(recordId) {
+  const records = getNestedRecordsExample();
+  const record = records.find((r) => r.id === recordId);
+  
+  if (record) {
+    return record.details.nestedInfo;
+  } else {
+    return null;
+  }
+}
+
+// Ejemplo de uso
+// const recordId = 2;
+// const nestedInfo = getNestedInfo(recordId);
+// console.log(nestedInfo);
 
 // { autofold
 module.exports = {
